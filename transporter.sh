4e3a1eec6611e365e1bdf24e91476e13771be71d
xSomoy <xsomoy@tutanota.com>
2019-10-19 11:42:36 +0600

    rewrite the whole so that others can use it too

--- transporter.sh
+++ transporter.sh
@@ -1,55 +1,81 @@
 #!/usr/bin/env bash
-## Note If Your
-echo "Starting Transfering Files"
-touch ~/transporterLOG
+echo "Im Just A Transporter"
+echo "Please Provide Nessesary Informations"
+date=$(date)
+touch ~/transporterReport
+echo "$date" >> ~/transporterReport
+task=0
 
-#Task 01
-echo "Starting Task 01" >> ~/transporterLOG
-rsync --progress -r [Source Directory] [Destination]
-echo "Task 01 Complere" >> ~/transporterLOG
-#Task 02
-echo "Starting Task 02" >> ~/transporterLOG
-rsync --progress -r [Source Directory] [Destination]
-echo "Task 02 Complere" >> ~/transporterLOG
-#Task 03
-echo "Starting Task 03" >> ~/transporterLOG
-rsync --progress -r [Source Directory] [Destination]
-echo "Task 03 Complere" >> ~/transporterLOG
-#Task 04
-echo "Starting Task 04" >> ~/transporterLOG
-rsync --progress -r [Source Directory] [Destination]
-echo "Task 04 Complere" >> ~/transporterLOG
-#Task 05
-echo "Starting Task 05" >> ~/transporterLOG
-rsync --progress -r [Source Directory] [Destination]
-echo "Task 05 Complere" >> ~/transporterLOG
-#Task 06
-echo "Starting Task 06" >> ~/transporterLOG
-rsync --progress -r [Source Directory] [Destination]
-echo "Task 06 Complere" >> ~/transporterLOG
-#Task 07
-echo "Starting Task 07" >> ~/transporterLOG
-rsync --progress -r [Source Directory] [Destination]
-echo "Task 07 Complere" >> ~/transporterLOG
-#Task 08
-echo "Starting Task 08" >> ~/transporterLOG
-rsync --progress -r [Source Directory] [Destination]
-echo "Task 08 Complere" >> ~/transporterLOG
+function permission() {
+    echo "Shutdown After Task(s) Completed? (y/n)" 
+    read input;
+    case $input in
+            y)  echo "echo \"All Task Are Done!\" >> ~/transporterReport " >> ~/tasklist.sh
+                echo "echo \"Initializing Computer ShutDown At \$(date) \" >> ~/transporterReport " >> ~/tasklist.sh
+                echo "echo \"Shuting Down Computer In 5 Seconds\" " >> ~/tasklist.sh
+                echo "sleep 1" >> ~/tasklist.sh
+                echo "echo \"Shuting Down Computer In 4 Seconds\" " >> ~/tasklist.sh
+                echo "sleep 1" >> ~/tasklist.sh
+                echo "echo \"Shuting Down Computer In 3 Seconds\" " >> ~/tasklist.sh
+                echo "sleep 1" >> ~/tasklist.sh
+                echo "echo \"Shuting Down Computer In 2 Seconds\" " >> ~/tasklist.sh
+                echo "sleep 1" >> ~/tasklist.sh
+                echo "echo \"Shuting Down Computer In 1 Seconds\" " >> ~/tasklist.sh
+                echo "sleep 1" >> ~/tasklist.sh
+                echo "echo \"Shuting Down Computer At \$(date) \" >> ~/transporterReport " >> ~tasklist.sh
+                echo "sleep 1" >> ~/tasklist.sh
+                echo "poweroff" >> ~/tasklist.sh
+                runtasks;;
+            n)  echo " echo \"All Task Are Done!\" >> ~/transporterReport " >> ~tasklist.sh
+                runtasks ;;
+            *) echo "Please Choose One Of Them"
+                    taskManager;;
+    esac
+}
 
-echo "Transfaring Data Is Finished" >> ~/transporterLOG
-clear
-echo "Transfaring Data Is Finished" 
-echo "Shuting Down Computer In 5 Seconds"
-echo "5"
-sleep 1
-echo "4"
-sleep 1
-echo "3"
-sleep 1
-echo "2"
-sleep 1
-echo "1"
-sleep 1
-echo "Shutting Down..."
-sleep 2
-poweroff
+function runtasks() {
+    cd ~
+    chmod +x tasklist.sh
+    ./tasklist.sh
+     rm tasklist.sh
+}
+
+function taskManager()  {
+    echo "Add Another Task? (y/n)"
+    read input;
+    case $input in
+            y) taskCreator;;
+            n)  echo "Total Task(s) = $task " >> ~/transporterReport
+                permission;;
+            *) echo "Please Choose One Of Them"
+                    taskManager;;
+    esac
+    
+}
+
+function taskCreator() {
+    echo "Transfer A File Or A Folder? "
+    echo "1 - File"
+    echo "2 - Folder"
+    read input;
+    case $input in
+            1) type="rsync --progress" ;;
+            2) type="rsync --progress -r" ;;
+            *) echo "Please Choose One Of Them"
+                    taskCreator;;
+    esac
+    echo "Full Path Of The Folder Or File is Nessesary"
+    echo "Source:"
+    read source;
+    echo "Destination:"
+    task=$(( $task+1 ))
+    read destination;
+    touch ~/tasklist.sh
+    echo "echo \"Starting Task $task\" " >> ~/tasklist.sh
+    echo "echo \"Starting Task $task\" >> ~/transporterReport " >> ~/tasklist.sh
+    echo "$type $source $destination" >> ~/tasklist.sh
+    echo "echo \"Task $task Is Done\" " >> ~/tasklist.sh
+    echo "echo \"Task $task Is Done\" >> ~/transporterReport " >> ~/tasklist.sh
+    taskManager
+}
+taskCreator
