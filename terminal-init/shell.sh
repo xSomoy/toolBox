ac4c3066d50ac932248fb3f16192f3f739a0be93
TENET <xSomoy@protonmail.com>
2025-02-20 00:02:12 +0600

    terminal-working

--- terminal-init/shell.sh
+++ terminal-init/shell.sh
@@ -1,3 +1,3 @@
-date= date +"%b%d-%a %H:%M"
+date +"%b%d-%a %H:%M"
 percent= upower -i $(upower -e | grep 'BAT') | grep "percentage" | awk -F':' '{print $2}' | tr -d ' '
-echo "$date$percent"
+echo "BAT: ${!percent}"
